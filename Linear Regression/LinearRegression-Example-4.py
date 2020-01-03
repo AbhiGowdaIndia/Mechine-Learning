5db98adf6530ff65c87e19af4f8980423d1c2c75
AbhiGowdaIndia <44654961+AbhiGowdaIndia@users.noreply.github.com>
2020-01-03 18:38:55 +0530

    Update LinearRegression-Example-4.py

--- Linear Regression/LinearRegression-Example-4.py
+++ Linear Regression/LinearRegression-Example-4.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('C:/Users/Admin/Desktop/ML/ML_programs/LinearReg/Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')
 x = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
